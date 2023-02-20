caf867dbcb7c97df7cafe9fd4a7d793fa910cc4e
Alexander Hewitson <alexander.hewitson@sky.uk>
2023-02-20 16:54:13 +0000

    feat(): add compat for custom elements

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
+import { defineCustomElements } from '../loader';
+import '../src/global/global.scss';
+
+defineCustomElements();
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
