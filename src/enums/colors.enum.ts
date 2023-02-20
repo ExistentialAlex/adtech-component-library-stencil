5178c8b8571f6e21fd5b37eff476b3eb573b90ae
Alexander Hewitson <alexander.hewitson@sky.uk>
2023-02-20 16:57:04 +0000

    feat(colors.enum): add all adtech colors

--- src/enums/colors.enum.ts
+++ src/enums/colors.enum.ts
@@ -17,9 +17,13 @@ export const AdTechNumericColors = {
   negative: 'negative',
 } as const;
 
-export type AdTechComponentColors =
-  typeof AdTechComponentColors[keyof typeof AdTechComponentColors];
-export type AdTechStatusColors =
-  typeof AdTechStatusColors[keyof typeof AdTechStatusColors];
-export type AdTechNumericColors =
-  typeof AdTechNumericColors[keyof typeof AdTechNumericColors];
+export const AllAdTechColors = {
+  ...AdTechComponentColors,
+  ...AdTechStatusColors,
+  ...AdTechNumericColors,
+};
+
+export type AdTechComponentColors = typeof AdTechComponentColors[keyof typeof AdTechComponentColors];
+export type AdTechStatusColors = typeof AdTechStatusColors[keyof typeof AdTechStatusColors];
+export type AdTechNumericColors = typeof AdTechNumericColors[keyof typeof AdTechNumericColors];
+export type AllAdTechColors = typeof AllAdTechColors[keyof typeof AllAdTechColors];
